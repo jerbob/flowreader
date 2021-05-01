2b9c82781058f42d411061100a657c86f7768a05
Jeremiah Boby <mail@jeremiahboby.me>
2021-05-01 14:14:04 +0100

    Reformat source files

--- flowreader/flowreader/flows/apps.py
+++ flowreader/flowreader/flows/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FlowsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'flows'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "flows"
