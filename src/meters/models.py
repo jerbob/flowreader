ce5cc10eaf13d30c9ea92f213a9430dbc86c788b
Jeremiah Boby <mail@jeremiahboby.me>
2021-05-01 19:12:05 +0100

    Add reading fields to MeterReading model

--- src/meters/models.py
+++ src/meters/models.py
@@ -6,14 +6,16 @@ from django.db import models
 class MeterReading(models.Model):
     """Represents a reading taken from a provided meter point."""
 
-    mpan = models.CharField(
+    mpan_number = models.CharField(
         max_length=21,
         verbose_name="MPAN",
         help_text="Meter Point Administration Number.",
     )
-    flow_file = models.CharField(
-        max_length=255, help_text="Flow file this reading was taken from."
-    )
     meter_serial_number = models.TextField(
         help_text="Serial number of the meter this reading was taken from."
+    )
+    reading = models.DecimalField(decimal_places=1, max_digits=255)
+    reading_datetime = models.DateTimeField()
+    flow_file = models.CharField(
+        max_length=255, help_text="Flow file this reading was taken from."
     )
