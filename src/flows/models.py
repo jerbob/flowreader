b2d5bd5165a649dc5e408dfa0f2c41b27cda24b9
Jeremiah Boby <mail@jeremiahboby.me>
2021-05-01 15:28:33 +0100

    Move verbose_name content to help_text field

--- src/flows/models.py
+++ src/flows/models.py
@@ -5,11 +5,13 @@ class MeterReading(models.Model):
     """Represents a reading taken from a provided meter point."""
 
     mpan = models.CharField(
-        max_length=21, verbose_name="Meter Point Administration Number."
+        max_length=21,
+        verbose_name="MPAN",
+        help_text="Meter Point Administration Number.",
     )
     flow_file = models.CharField(
-        max_length=255, verbose_name="Flow file this reading was taken from."
+        max_length=255, help_text="Flow file this reading was taken from."
     )
     meter_serial_number = models.TextField(
-        verbose_name="Serial number of the meter this reading was taken from."
+        help_text="Serial number of the meter this reading was taken from."
     )
