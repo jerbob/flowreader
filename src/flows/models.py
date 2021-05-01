77c64fa9d4a9ff8496f2c9689259ea7df1c1d377
Jeremiah Boby <mail@jeremiahboby.me>
2021-05-01 15:22:12 +0100

    Create the 'MeterReading' model

--- src/flows/models.py
+++ src/flows/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class MeterReading(models.Model):
+    """Represents a reading taken from a provided meter point."""
+
+    mpan = models.CharField(
+        max_length=21, verbose_name="Meter Point Administration Number."
+    )
+    flow_file = models.CharField(
+        max_length=255, verbose_name="Flow file this reading was taken from."
+    )
+    meter_serial_number = models.TextField(
+        verbose_name="Serial number of the meter this reading was taken from."
+    )
